6b3c59fef171d507400cc490fc7e9a528ff9c9df
github-marynapoliarush <merienf8118@gmail.com>
2023-08-30 12:22:12 +0300

    first commit

---
+++ parserIndex.js
@@ -0,0 +1,64 @@
+console.log("helloIndex");
+const puppeteer =  require('puppeteer-extra');
+var kill  = require('tree-kill');
+
+async function getIndex (){
+    let stock={
+        price:'0',
+        daily:'0',
+        weekly:'0'
+    }
+  
+    try{
+  
+        const browser = await puppeteer.launch({headless:true, 'ignoreDefaultArgs': [ '--enable-automation'] })
+        const newPage = await browser.newPage();
+        await newPage.goto('https://tradingeconomics.com/stocks',{timeout: 0})
+
+
+        // await newPage.click('didomi-notice-agree-button')
+
+            let allStock = await newPage.evaluate( ()=>{
+                const stockArray = Array.from(document.querySelectorAll('#p'), e => e.innerText)
+                
+                return stockArray
+            })
+            stock.price = allStock.slice(0,3)
+
+        
+            let allday = await newPage.evaluate( ()=>{
+                const nums = Array.from(document.querySelectorAll('#pch'), e => e.innerText)
+                return nums
+            })
+          
+
+            stock.daily = allday.slice(0,3)
+
+
+
+            let allweek = await newPage.evaluate( ()=>{
+                const nums = Array.from(document.querySelectorAll('.datatable-item.datatable-heatmap '), e => e.innerText)
+            
+                return nums
+            })
+            stock.weekly = allweek.slice(0,9)
+
+            // console.log(stock)
+
+        await browser.close()
+        
+    
+
+}catch(e){
+    console.log(e)
+
+}
+  
+    // console.log(stock)
+    return stock
+    
+}
+
+getIndex()
+
+module.exports = {getIndex}
---
+++ parserRate.js
@@ -0,0 +1,46 @@
+console.log("hello rates");
+const puppeteer =  require('puppeteer-extra');
+
+
+async function getRate (){
+    let currencies
+
+  try{
+      const browser = await puppeteer.launch({"headless": true ,"args": ["--fast-start", "--disable-extensions", "--no-sandbox"], 'ignoreDefaultArgs': [ '--enable-automation'] })
+      const newPage = await browser.newPage();
+      await newPage.goto('https://finance.ua/ua/currency',{timeout: 0})
+
+    
+          let allNums = await newPage.evaluate( ()=>{
+              const nums = Array.from(document.querySelectorAll('.c2 '), e => e.innerText)
+              return nums
+          })
+          
+          
+          currencies = allNums.slice(2,4)
+
+          let allNums2 = await newPage.evaluate( ()=>{
+            const nums = Array.from(document.querySelectorAll('.c3 '), e => e.innerText)
+            return nums
+        })
+        
+        
+        currencies.push(...allNums2.slice(2,4))
+      
+
+      await browser.close()
+  }catch(e){
+      console.log(e)
+  }
+
+ 
+//   console.log(currencies)
+    return currencies
+    
+}
+
+
+
+
+
+module.exports = {getRate}
